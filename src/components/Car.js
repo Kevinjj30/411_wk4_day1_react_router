c40d44654488014048ae588d1865fee9ec9b8f4f
Kevinjj30 <kevinjenkins56@yahoo.com>
2020-02-02 21:43:57 -0600

    homeowork done

--- src/components/Car.js
+++ src/components/Car.js
@@ -9,14 +9,23 @@ let id = props.match.params.id;
 let thisCar = cars.find(car => id == car.id)
 
 return (
+    <div classname ="bodytest">
        <div classname ="car-container" key={props.match.params.id}>
 
     <Container>
-        <Paper>
+        <Paper elevation={3} >
         <Typography variant='h4'>{`Name: ${thisCar.Name}`}</Typography>
         <Chip label={`Miles per gallon: ${thisCar.Miles_per_Gallon}`}></Chip>
+        <Chip label={`Cylinders: ${thisCar.Cylinders}`}></Chip>
+        <Chip label={`Displacement: ${thisCar.Displacement}`}></Chip>
+        <Chip label={`Horsepower: ${thisCar.Horsepower}`}></Chip>
+        <Chip label={`Weight (lbs): ${thisCar.Weight_in_lbs}`}></Chip>
+        <Chip label={`Acceleration: ${thisCar.Acceleration}`}></Chip>
+        <Chip label={`Year: ${thisCar.Year}`}></Chip>
+        <Chip label={`Country of Origin: ${thisCar.Origin}`}></Chip>
     </Paper>
     </Container>
+</div>
 </div>
     )
     
